c3b6ac7328f2d105dbdd6b92eb31f7380aa110e8
daeho0818 <daeho0818@naver.com>
2021-11-17 08:46:57 +0900

    응애 ParticleManager

--- ParticleManager.cpp
+++ ParticleManager.cpp
@@ -8,3 +8,54 @@ ParticleManager::ParticleManager()
 ParticleManager::~ParticleManager()
 {
 }
+
+void ParticleManager::Update()
+{
+	for(var iter : )
+}
+
+void ParticleManager::Render()
+{
+}
+
+void ParticleManager::Release()
+{
+}
+
+void ParticleManager::AddParticleAnim(vector<Image*> animation, Vector2 position, float frameTime, float size)
+{
+	Particle* particle = new Particle(animation, position, frameTime, size);
+	m_particles.push_back(particle);
+}
+
+void ParticleManager::AddParticleEffect(Image* image, Vector2 position, D3DXCOLOR color, ChangeMode changeMode, float size, float destroyTime)
+{
+	Effect* effect = new Effect(image, position, color, changeMode, size, destroyTime);
+	m_effects.push_back(effect);
+}
+
+Particle::Particle(vector<Image*> animation, Vector2 position, float frame_time, float size)
+{
+	m_animation = animation;
+	m_position = position;
+	m_frameTime = frame_time;
+	m_size = size;
+	m_index = 0;
+	m_destroy = false;
+}
+
+Particle::~Particle()
+{
+}
+
+Effect::Effect(Image* image, Vector2 position, D3DXCOLOR color, ChangeMode change_mode, float size, float destroy_time)
+{
+	m_image = image;
+	m_color = color;
+	m_changeMode = change_mode;
+	m_destroyTime;
+}
+
+Effect::~Effect()
+{
+}
--- ParticleManager.h
+++ ParticleManager.h
@@ -1,9 +1,69 @@
 #pragma once
 #include "Singleton.h"
+
+enum ChangeMode
+{
+	Small = 1,
+	Big = 2,
+	FadeIn = 4,
+	FadeOut = 8,
+
+	Small_FadeIn = 5,
+	Small_FadeOut = 9,
+
+	Big_FadeIn = 6,
+	Big_FadeOut = 10
+};
+
 class ParticleManager : public Singleton<ParticleManager>
 {
 public:
 	ParticleManager();
 	~ParticleManager();
+
+	void Update();
+	void Render();
+	void Release();
+
+	void AddParticleAnim(vector<Image*> animation, Vector2 position, float frameTime, float size = 1);
+	// changeMode = ChangeMode::Big | ChangeMode::FadeOut 이런식으로 적용
+	void AddParticleEffect(Image* image, Vector2 position, D3DXCOLOR color, ChangeMode changeMode, float size = 1, float destroyTime = 1);
+
+private:
+	vector<Particle*> m_particles;
+	vector<Effect*> m_effects;
+
+	bool isParticleAnimation;
+	bool isParticleEffect;
 };
 #define PARTICLE ParticleManager::Instance()
+
+class Particle
+{
+public:
+	Particle(vector<Image*> animation, Vector2 position, float frame_time, float size);
+	~Particle();
+
+private:
+	vector<Image*> m_animation;
+	Vector2 m_position;
+	float m_frameTime;
+	float m_size;
+	int m_index;
+	bool m_destroy;
+};
+
+class Effect
+{
+public:
+	Effect(Image* image, Vector2 position, D3DXCOLOR color, ChangeMode change_mode, float size, float destroy_time);
+	~Effect();
+
+private:
+	Image* m_image;
+	Vector2 m_position;
+	D3DXCOLOR m_color;
+	ChangeMode m_changeMode;
+	float m_size;
+	float m_destroyTime;
+};

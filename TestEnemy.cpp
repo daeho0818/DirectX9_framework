f51a443c97012348e3f8fc5af856899a46a7c92e
daeho0818 <daeho0818@naver.com>
2021-12-06 20:37:14 +0900

    TestEnemy 클래스로 적 테스트 해볼 것

---
+++ TestEnemy.cpp
@@ -0,0 +1,31 @@
+#include "DXUT.h"
+#include "TestEnemy.h"
+
+TestEnemy::TestEnemy(Object* object)
+	:Component(object)
+{
+}
+
+TestEnemy::~TestEnemy()
+{
+}
+
+void TestEnemy::Init()
+{
+}
+
+void TestEnemy::Update()
+{
+}
+
+void TestEnemy::Render()
+{
+}
+
+void TestEnemy::UIRender()
+{
+}
+
+void TestEnemy::Release()
+{
+}
---
+++ TestEnemy.h
@@ -0,0 +1,15 @@
+#pragma once
+class TestEnemy : public Component
+{
+public :
+	TestEnemy(Object* object);
+	~TestEnemy();
+
+	// Component을(를) 통해 상속됨
+	virtual void Init() override;
+	virtual void Update() override;
+	virtual void Render() override;
+	virtual void UIRender() override;
+	virtual void Release() override;
+};
+
